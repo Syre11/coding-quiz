7da05dc713198ad2607c0b674399f5064fa8184b
“Spencer <srye1140@gmail.com>
2022-10-19 18:07:47 -0700

    Adds most of the needed html and css, adds hooks and a few lines of code to js, adds sample outline to read-me

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var quizTitleEl = document.querySelector(".quizTitle")
+var quizInstrEl = document.querySelector(".quizInstr")
+var questionEl = document.querySelector(".question")
+var answersEl = document.querySelector(".answers")
+var startButton = document.querySelector(".startBtn")
+
+
+// <!-- ***********************************************************
+//   *************************************// *****************************
+//   *******************************************************************
+//   *****DON'T FORGET TO ADD A SCREENSHOT AND LINK TO YOUR README******
+//   **********************************************************
+//   ***********************************************************
+//   ********************************************************** -->
+
+
+startButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  // needs to start the timer and pull up the first questions and it's choice of answers
+})
